420f1b3c7574b4e3866d734f7864b8c2843ac2cd
Rafael Paz <rrafaelpaz@gmail.com>
2018-05-18 19:29:36 +1000

    main class modified

--- src/main/java/com/test/MainClass.java
+++ src/main/java/com/test/MainClass.java
@@ -1,10 +1,14 @@
 package com.test;
 
+import javax.swing.JOptionPane;
+
 public class MainClass {
 
 	public static void main(String args[]){
 		
 		
 		System.out.println("This is a test of the main class on circleci");
+		
+		JOptionPane.showMessageDialog(null, "Project running");
 	}
 }
