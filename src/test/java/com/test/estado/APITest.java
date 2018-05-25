158168a05eb8ae68c1a018563eef5ce68b4907e3
Rafael Paz <rrafaelpaz@gmail.com>
2018-05-26 09:46:03 +1000

    karate with gradle test

---
+++ src/test/java/com/test/estado/APITest.java
@@ -0,0 +1,15 @@
+package com.test.estado;
+
+
+import com.intuit.karate.junit4.Karate;
+import cucumber.api.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Karate.class)
+@CucumberOptions(features = "src/test/resources") 
+public class APITest {
+	
+
+
+	
+}
